6fe5693075dff48f27162dbfb237d395757a9bc4
rquesada <rquesadaqq@outlook.com>
2026-03-01 17:27:36 -0600

    Add custom AppError class

---
+++ src/utils/AppError.js
@@ -0,0 +1,14 @@
+/**
+ * Clase de error personalizado para la aplicación
+ */
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
